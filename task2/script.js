edf57b7c42ba3dc7dbea155065948c66aedca098
Pavel Katkovsky <PavelKatkovsky@coherentsolutions.com>
2016-03-29 10:14:18 +0300

    task2

---
+++ task2/script.js
@@ -0,0 +1,38 @@
+var app = angular.module('task2', []);
+
+app.component('tabControl', {
+    transclude: true,
+    controller: function() {
+        var panes = this.panes = [];
+        this.select = function(pane) {
+            angular.forEach(panes, function(pane) {
+                pane.selected = false;
+            });
+            pane.selected = true;
+        };
+        this.addPane = function(pane) {
+            if (panes.length === 0) {
+                this.select(pane);
+            }
+            panes.push(pane);
+        };
+    },
+    templateUrl: 'tabs.html'
+});
+
+app.component('paneControl', {
+    transclude: true,
+    require: {
+        tabsCtrl: '^tabControl'
+    },
+    bindings: {
+        title: '@'
+    },
+    controller: function() {
+        this.$onInit = function() {
+            this.tabsCtrl.addPane(this);
+            console.log(this);
+        };
+    },
+    templateUrl: 'pane.html'
+});
